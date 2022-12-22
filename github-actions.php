19a1f1a05e4e6d728f5964c12b680391ecbafd6b
John Regan <john.m.regan@gmail.com>
2022-12-22 07:53:15 -0600

    Initial commit

---
+++ github-actions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: GitHub Actions
+ * Plugin URI:
+ * Description: GitHub Actions Test Plugin
+ * Version: 1.0.0
+ * Author: GitHub
+ * Author URI:
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: github-actions
+ * Domain Path: /languages
+ *
+ * @package GitHubActions
+ */
+
+add_action(
+	'admin_notices',
+	function () {
+		?>
+		<div class="notice notice-success is-dismissible">
+			<p><?php esc_html_e( 'Done!', 'github-actions' ); ?></p>
+		</div>
+		<?php
+	}
+);
