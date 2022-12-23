cdc55955028090919ccea9fd451078d403743e78
John Regan <john.m.regan@gmail.com>
2022-12-23 10:02:01 -0600

    add wpcs error

--- github-actions.php
+++ github-actions.php
@@ -19,7 +19,7 @@ add_action(
 	function () {
 		?>
 		<div class="notice notice-error">
-			<p><?php esc_html_e( 'GITHUB ACTIONS PLUGIN IS WORasdfasfKIgn!!', 'github-actions' ); ?></p>
+			<p><?php esc_html_e('GITHUB ACTIONS PLUGIN IS WORasdfasfKIgn!!', 'github-actions'); ?></p>
 		</div>
 		<?php
 	}
