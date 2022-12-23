f458054af9bc0eb2dced9fc7e3b7581b2ff02cd7
John Regan <john.m.regan@gmail.com>
2022-12-23 12:11:12 -0600

    Add finder to stupid thing

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,10 @@
 <?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('somedir')
+    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
+    ->in(__DIR__)
+;
  $config = new PhpCsFixer\Config();
  return $config->setRules([
 		 '@PSR12' => true,
