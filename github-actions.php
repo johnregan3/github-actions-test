284cbde25869b4c4ecd0098a9accdb9c91b5820f
John Regan <john.m.regan@gmail.com>
2023-01-12 10:40:13 -0600

    I dunno

--- github-actions.php
+++ github-actions.php
@@ -20,8 +20,8 @@ add_action(
 
 		?>
 		<div class="notice notice-error">
-			<?php if ( $_GET['TESTING'] && in_array( 'this', array( 'something', 'something_else' ) ) ) : ?>
-				<p><?php esc_html_e(  'this is the plugin', 'github-actions' ); ?></p>
+			<?php if ( isset( $_GET['TESTING'] ) && in_array( $_GET['TESTING'], array( 'this', 'something_else' ) ) ) : ?>
+				<p><?php esc_html_e( 'this is the plugin', 'github-actions' ); ?></p>
 			<?php endif; ?>
 		</div>
 		<?php
